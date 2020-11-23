617fdb9c1cf4f2006adb53768ef9ea245010d383
maxospiquante <maxence.lecointe@free.fr>
2020-11-23 20:31:55 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators import MultiplyBy5Operator
+
 
 def print_hello():
  return 'Hello Wolrd'
